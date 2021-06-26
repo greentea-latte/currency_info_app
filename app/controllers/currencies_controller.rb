bdffea637f461976fb10ca1326596be7952de10b
Lila Liu <lilaliu@Lilas-Air.broadband>
2021-06-26 09:51:51 +0100

    Add a dictionary class for country and currency data
    Update dropdowns to include additional countries

--- app/controllers/currencies_controller.rb
+++ app/controllers/currencies_controller.rb
@@ -1,38 +1,36 @@
+require_relative '../../db/dictionary'
+
 class CurrenciesController < ApplicationController
     def index
-        if params["amount"].present? && params["currency_to_convert"].present?
-            currency = params["currency_to_convert_to"].to_sym
-            amount = params["amount"].to_i
-            @rate = rates_for_one_euro[currency].to_i * amount
+        @countries_for_select = countries_for_select
+        @currencies_for_select = currencies_for_select
+        @currencies_for_conversion = currencies_for_conversion
+        country_to_currencies_map = Dictionary.country_to_currencies
+        currency_to_countries_map = Dictionary.currency_to_countries
+        
+        if params["amount"].present? && params["convert_from"].present? && params["convert_to"].present?
+            i = FixerApi::CurrencyConversion.new(from: params["convert_from"], to: params["convert_to"], amount: params["amount"].to_i)
+            @rate = i.convert
         elsif params["country"].present?
-            country = params["country"].to_sym
-            @currencies = currencies[country]
+            country = params["country"]
+            @currencies = country_to_currencies_map[country]
         elsif params["currency"].present?
-            currency = params["currency"].to_sym
-            @countries = countries[currency]
+            currency = params["currency"]
+            @countries = currency_to_countries_map[currency]
         end
     end
 
     private
 
-    def rates_for_one_euro
-        {
-            "Peso": 57
-        }
+    def currencies_for_conversion
+        Dictionary.currencies.map { |currency| [currency, currency] }
     end
 
-    def currencies
-        {
-            "France": ["Euro"],
-            "Mexico": ["Peso"]
-        }
+    def countries_for_select
+        Dictionary.country_to_currencies.map { |country, currencies| [country, country] }
     end
 
-    def countries
-        {
-            "Euro": ["France", "Spain", "Germany"],
-            "Peso": ["Mexico"]
-
-        }
+    def currencies_for_select
+        Dictionary.currency_to_countries.map { |currency, countries| [currency, currency] }
     end
 end
